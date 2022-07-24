f0639fd5c96bbc6c625ded54e01ab351436c6169
davidyoon <qqpo12@naver.com>
2022-07-24 22:50:04 +0900

    Add defauls compoents
    * Added Borad, Profile components
    * Set a default href

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
 
+import Profile from "./Profile"
+import Board from "./Board"
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <nav>
+        <ul>
+          <li>
+            <a href="profile">프로필</a>
+          </li>
+          <li>
+            <a href="board">게시판</a>
+          </li>
+        </ul>
+      </nav>
+      <Profile />
+      <Board />
     </div>
   );
 }
---
+++ src/Board.js
@@ -0,0 +1,5 @@
+export default function Board() {
+    return (
+        <div>게시판 페이지</div>
+    )
+}
